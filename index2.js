0e50e496e3e26ca78b76ddfd6219e25f9f47db19
kiyo4810 <kiyo4810@gmail.com>
2025-12-31 12:30:38 +0900

    12/31 完了！

---
+++ index2.js
@@ -0,0 +1,9 @@
+//変数nameを宣言し私の名前を底に入れる
+const myName = "きよひこ";
+function sayHello(userName){
+    return "こんにちは、" + userName + "さん！Githubの学習をはじめましょう";
+}
+const message = sayHello(myName);
+console.log(message);
+console.log("コンソールに表示される文字だよ");
+console.log("大晦日になにしとんねんおまえ！");
