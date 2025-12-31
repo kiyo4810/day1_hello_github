6c331298438537aab6e86177fc0b832202423df2
kiyo4810 <kiyo4810@gmail.com>
2025-12-31 20:54:57 +0900

    d

--- index2.js
+++ index2.js
@@ -1,12 +1,12 @@
 //変数nameを宣言し私の名前をそこに入れる
-const myName = "きよひこまる";
+const thecatName = "ごまちゃん";
 //関数は「特定の処理をまとめた命令セット」です。
 //userName は**引数（ひきすう）**と呼ばれ、関数を動かすときに外から渡されるデータを受け取る窓口になります。
 //return（戻り値）は、この関数が実行された結果として「外に返す値」を指します。ここでは、挨拶文を作って返しています。
-function makeSentence(name){
-    return "こんにちは、" + name + "で美味しい炊きたてご飯をつくるよ";
+function makeSentence(catName){
+    return "ゆいいつ" + catName + "だけがかわいい猫だね";
 }
-const message = makeSentence(myName);
+const message = makeSentence(thecatName);
 console.log(message);
 console.log(message);
 console.log("コンソールに表示される文字だよ");
