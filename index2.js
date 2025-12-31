5c586b2b5912848cda9b9f6d96190b9145b6b1ef
kiyo4810 <kiyo4810@gmail.com>
2025-12-31 21:04:08 +0900

    コンソールアホ猫の塾

--- index2.js
+++ index2.js
@@ -8,6 +8,17 @@ function makeSentence(catName){
 }
 const message = makeSentence(thecatName);
 console.log(message);
-console.log(message);
+console.log("コンソールに表示される文字だよ");
+console.log("大晦日になにしとんねんおまえ！");
+
+const theahoCat = "パンくん";
+//関数は「特定の処理をまとめた命令セット」です。
+//userName は**引数（ひきすう）**と呼ばれ、関数を動かすときに外から渡されるデータを受け取る窓口になります。
+//return（戻り値）は、この関数が実行された結果として「外に返す値」を指します。ここでは、挨拶文を作って返しています。
+function jyuku(catName){
+    return "我が塾はアホ猫、" + catName + "を賢い猫にします」";
+}
+const message2 = jyuku(theahoCat);
+console.log(message2);
 console.log("コンソールに表示される文字だよ");
 console.log("大晦日になにしとんねんおまえ！");
