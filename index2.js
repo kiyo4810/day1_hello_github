23fbe5cdd60b9d855ec635843748707df01d76a4
kiyo4810 <kiyo4810@gmail.com>
2025-12-31 20:48:28 +0900

    もとに戻す自分の言葉で

--- index2.js
+++ index2.js
@@ -1,12 +1,13 @@
 //変数nameを宣言し私の名前をそこに入れる
-const zairyo = "おコメ";
+const myName = "きよひこまる";
 //関数は「特定の処理をまとめた命令セット」です。
 //userName は**引数（ひきすう）**と呼ばれ、関数を動かすときに外から渡されるデータを受け取る窓口になります。
 //return（戻り値）は、この関数が実行された結果として「外に返す値」を指します。ここでは、挨拶文を作って返しています。
-function suihanMachine(kama){
-    return "こんにちは、" + kama + "で美味しい炊きたてご飯をつくるよ";
+function makeSentence(name){
+    return "こんにちは、" + name + "で美味しい炊きたてご飯をつくるよ";
 }
-const message = suihanMachine(zairyo);
+const message = makeSentence(myName);
+console.log(message);
 console.log(message);
 console.log("コンソールに表示される文字だよ");
 console.log("大晦日になにしとんねんおまえ！");
