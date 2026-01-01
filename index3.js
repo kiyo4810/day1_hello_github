4a78545f3f41b34d15a8d27fd7b11d454bf7f047
kiyo4810 <kiyo4810@gmail.com>
2026-01-01 22:06:37 +0900

    m02とm03を追記

--- index3.js
+++ index3.js
@@ -1,8 +1,27 @@
-const thecat = "パン";
+//猫塾
+const thecat = "パンくん";
 console.log(thecat);
 function catjuku(cat){
     return "この塾は" + cat +"を賢い猫にします";
 }
-const message = catjuku(thecat);
-console.log(message);
-document.getElementById("m01").innerText = message;
+const message1 = catjuku(thecat);
+console.log(message1);
+document.getElementById("m01").innerText = message1;
+
+//猫保育園
+const theshyCat = "ごまくん";
+function hoikuen(cat){
+    return "この保育園は" + cat + "を甘やかします";
+}
+const message2 = hoikuen(theshyCat);
+console.log(message2);
+document.getElementById("m02").innerText = message2;
+
+//猫天国
+const theheavenCat = "レオくん";
+function heaven(cat){
+    return "ここが" + cat + "がいる天国だよ";
+}
+const message3 = heaven(theheavenCat);
+console.log(message3);
+document.getElementById("m03").innerText =message3;
