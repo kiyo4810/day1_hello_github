eb893b064041d6492c51fdcd658a14ea1612b39a
kiyo4810 <kiyo4810@gmail.com>
2026-01-01 21:49:32 +0900

    練習index3のhtmlとjs

---
+++ index3.js
@@ -0,0 +1,8 @@
+const thecat = "パン";
+console.log(thecat);
+function catjuku(cat){
+    return "この塾は" + cat +"を賢い猫にします";
+}
+const message = catjuku(thecat);
+console.log(message);
+document.getElementById(m01).innerText = message;
